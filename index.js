1ebf75c9da44a20853f03944e283853f3af6ca38
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2019-09-23 15:35:05 -0500

    nodejs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
-var io = require('../..')(server);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 server.listen(port, () => {
@@ -12,7 +12,9 @@ server.listen(port, () => {
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 // Chatroom
 
 var numUsers = 0;
